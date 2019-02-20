40aa67684ae9b244c1da03e4e01fd371ae68b9af
KE Clements <31896853+keclements@users.noreply.github.com>
2019-02-19 22:03:28 -0500

    uploade issues

---
+++ palindrome.py
@@ -0,0 +1,14 @@
+# define[function_name][(parameter)]:
+    # [function+definition]
+
+# Tasks
+    # ask user for one or more sentences
+    # evaluate the user input using python script
+    # let use know if their input is a palindrome
+
+# user inputs a string argument 
+
+def is_palindrome():
+    """evaluate if the input
+ 
+# read input 
